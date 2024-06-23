092944569c3c77b563e84ea4403162a4676e762f
wesleywmgMMU <123866358+wesleywmgMMU@users.noreply.github.com>
2024-06-23 15:30:26 +0800

    Add files via upload

--- assignment/settings.py
+++ assignment/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'assignment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
